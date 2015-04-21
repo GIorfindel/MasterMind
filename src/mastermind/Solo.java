08807da732301c91f8a133b3dd6de4967b338c74
saifeddineb7 <saifeddineb7@gmail.com>
2015-04-21 19:19:44 +0200

    Create Solo.java
    La structure Solo permet de jouer tout seul au Mastermind

---
+++ src/mastermind/Solo.java
@@ -0,0 +1,20 @@
+package mastermind;
+
+public class Solo {
+	
+	/* Variables */
+	private String nom; //Nom de la partie
+	private Niveau niveau;
+	private Joueur joueur;
+	private Integer Coups; //le nombre de coup que le joueur a fait
+	private Tour tour; //le tour que l’on fait
+	
+
+
+public void setCoups( Integer coups ) {
+		this.coups = coups;
+	}
+	
+public Coups getCoups() {
+		return this.coups;
+	}
