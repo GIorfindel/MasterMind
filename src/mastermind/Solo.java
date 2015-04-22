fabef9ce198720c0a69a6cb9b4e4f651d2f558fd
saifeddineb7 <saifeddineb7@gmail.com>
2015-04-22 22:40:04 +0200

    Update Solo.java

--- src/mastermind/Solo.java
+++ src/mastermind/Solo.java
@@ -1,23 +1,12 @@
 package mastermind;
 
-public class Solo {
+public class Solo extends Partie{
 	
 	/* Variables */
-	private String nom; //Nom de la partie
-	private Niveau niveau;
-	private Joueur joueur;
-	private Integer Coups; //le nombre de coup que le joueur a fait
 	private Tour tour; //le tour que l’on fait
 	
 
-
-public void setCoups( Integer coups ) {
-		this.coups = coups;
-	}
-	
 public Coups getCoups() {
-		return this.coups;
+		return super.coups;
 	}
-
-	
 }
