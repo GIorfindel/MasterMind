879c26f7a127f10d39052d08c1c66ed80d2269e5
ChristGood <christ.good@yahoo.fr>
2015-04-24 00:48:52 +0200

    Soucis d'encodage résolues

--- src/online/Client.java
+++ src/online/Client.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Client {
 
-	protected static final int port = 15000; //M�me port que le serveur
+	protected static final int port = 15000; //Même port que le serveur
 	protected static Socket socket = null; // Initialisation du socket clien
 	protected static Thread t1; // Initialisation du premier thread 
 	protected static PrintWriter sortie = null;
@@ -21,19 +21,19 @@ public class Client {
 		String identifiant = "tata"; // L'identifiant est normalement fourni par le programme
 		
 		System.out.println("Demande de connexion");
-		socket = new Socket("127.0.0.1",port); // Cr�ation d'un nouveau socket � l'adresse et au port donn�s
+		socket = new Socket("127.0.0.1",port); // Création d'un nouveau socket à l'adresse et au port donnés
 		sortie = new PrintWriter(socket.getOutputStream());
 		sortie.println(identifiant);
 		sortie.println("test");
 		sortie.flush();
 		
-		//Apr�s acceptation par le serveur (socketserveur.accept())
-		System.out.println("Connexion �tablie avec le serveur"); // Si le message s'affiche c'est que je suis connect�	
+		//Après acceptation par le serveur (socketserveur.accept())
+		System.out.println("Connexion établie avec le serveur"); // Si le message s'affiche c'est que je suis connecté	
 		
 	} catch (UnknownHostException e) {
-	  System.err.println("Impossible de se connecter � cette adresse");
+	  System.err.println("Impossible de se connecter à cette adresse");
 	} catch (IOException e) {
-	  System.err.println("Aucun serveur � l'�coute de ce port");
+	  System.err.println("Aucun serveur à l'écoute de ce port");
 	}
 	
 	
--- src/online/Serveur.java
+++ src/online/Serveur.java
@@ -6,9 +6,9 @@ import java.util.*;
 
 public class Serveur {
 	protected static final int port = 15000; // Port sur lequel se connecter
-	protected static ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'�coute du port
-	protected static Thread accepte_connexion; // D�claration d'un thread
-	protected ArrayList<Socket> listeSocket; // Liste contenant les infos r�seau
+	protected static ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'écoute du port
+	protected static Thread accepte_connexion; // Déclaration d'un thread
+	protected ArrayList<Socket> listeSocket; // Liste contenant les infos réseau
 	protected ArrayList<String> listeJoueurs; //Liste contenant les identifiants
 	protected int nbJoueurs = 0; 
 	
@@ -34,75 +34,75 @@ public class Serveur {
 	public static void main(String[] args) {
 		
 		try {
-			socketServeur = new ServerSocket(port); // Cr�ation d'un socket d'�coute sur le port donn�e
+			socketServeur = new ServerSocket(port); // Création d'un socket d'écoute sur le port donnée
 			Serveur serveur = new Serveur(); // Initialisation des ArrayList
-			System.out.println("Le serveur est � l'�coute du port "+socketServeur.getLocalPort());
+			System.out.println("Le serveur est à l'écoute du port "+socketServeur.getLocalPort());
 			
-			/* Cr�ation d'un nouveau thread appel� accepte_connexion, dont la t�che � effectuer (la m�thode run() ) 
+			/* Création d'un nouveau thread appelé accepte_connexion, dont la tâche à effectuer (la méthode run() ) 
 			 * se trouve dans la classe Accepter_connexion
-			 * qui est elle-m�me une impl�mentation de runnable (voir tuto openclassroom)
+			 * qui est elle-même une implémentation de runnable (voir tuto openclassroom)
 			 */
 			accepte_connexion = new Thread(serveur.new Accepter_connexion(socketServeur));
 			accepte_connexion.start();
 				
 		} catch (IOException e) {
-			System.err.println("Le port " + socketServeur.getLocalPort() + " est d�j� utilis� !");
+			System.err.println("Le port " + socketServeur.getLocalPort() + " est déjà utilisé !");
 		}
 		
 	}
 		
 	
-	/*Classe membre qui contient ce que doit ex�cuter le thread
-	 *Si �a vous pertubre, vous pouvez cr�er un nouveau fichier et faire un couper/coller.
-	 *Si vous faites �a, n'oubliez pas la port�e des variables.
+	/*Classe membre qui contient ce que doit exécuter le thread
+	 *Si ça vous pertubre, vous pouvez créer un nouveau fichier et faire un couper/coller.
+	 *Si vous faites ça, n'oubliez pas la portée des variables.
 	 */
 	class Accepter_connexion implements Runnable{
 	
-		private ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'�coute du port
-		private Socket socketClient = null; // Socket qui s'occupe de l'�change de flux avec le client
+		private ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'écoute du port
+		private Socket socketClient = null; // Socket qui s'occupe de l'échange de flux avec le client
 		private PrintWriter sortie = null;
-		private BufferedReader entree = null; // L'entr�e sera (presque) toujours lue en utilisant un buffer
+		private BufferedReader entree = null; // L'entrée sera (presque) toujours lue en utilisant un buffer
 		private String identifiant = null;
 		
-		// R�cup�ration du socket de serveur de la classe Serveur
+		// Récupération du socket de serveur de la classe Serveur
 		public Accepter_connexion(ServerSocket ss) {
 			socketServeur = ss;
 		}
 		
-		// D�marrage du thread accepte_connexion
+		// Démarrage du thread accepte_connexion
 		public void run() {
 			
 			try {
-				while(nbJoueurs < 5){ // Normalement c'est un while(true), ou un autre pr�dicat
+				while(nbJoueurs < 5){ // Normalement c'est un while(true), ou un autre prédicat
 					
 					socketClient = socketServeur.accept(); // On accepte la connexion
 					
 					entree = new BufferedReader(new InputStreamReader(socketClient.getInputStream())); //Initialisation du buffer sur le socketClient
 					sortie = new PrintWriter(socketClient.getOutputStream()); // Initialisation du flux de sortie
 					
-					listeSocket.add(socketClient); // On ajoute le socket dans la liste (peut �tre utile pour la suite)
+					listeSocket.add(socketClient); // On ajoute le socket dans la liste (peut être utile pour la suite)
 					
 					identifiant = entree.readLine(); // On lit le sortie.println du Client
 					
 					listeJoueurs.add(identifiant); // On ajoute l'identifiant du client dans la liste
 					
-					System.out.println(identifiant+" vient de se connecter"); // Affichage d'un message c�t� Serveur
+					System.out.println(identifiant+" vient de se connecter"); // Affichage d'un message côté Serveur
 					/* Si on voulait envoyer un message chez le client, on aurait fait :
-					 * sortie.println("blabla") c�t� Serveur
-					 * sortie.flush() c�t� Serveur pour vider le buffer
-					 * message = entree.readLine() c�t� Client
+					 * sortie.println("blabla") côté Serveur
+					 * sortie.flush() côté Serveur pour vider le buffer
+					 * message = entree.readLine() côté Client
 					 */
 					
 					nbJoueurs++;
 				}
 				afficheSocket(listeSocket);
 				afficheLogin(listeJoueurs);
-				System.out.println("Il y a "+nbJoueurs+" joueurs connect�s"); 
+				System.out.println("Il y a "+nbJoueurs+" joueurs connectés"); 
 				
 				
-				/* Fermeture du socket � la FIN du programme. Obligatoire pour lib�rer le port.
-				 * Cette m�thode n'est utile que si on souhaite fermer le serveur, 
-				 * ce qui serait rare dans des conditions r�elles */
+				/* Fermeture du socket à la FIN du programme. Obligatoire pour libérer le port.
+				 * Cette méthode n'est utile que si on souhaite fermer le serveur, 
+				 * ce qui serait rare dans des conditions réelles */
 				socketServeur.close(); 
 				
 				
