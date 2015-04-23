d8149ce8331d1ab1aba0eae2c0318f8abc74163e
ChristGood <christ.good@yahoo.fr>
2015-04-23 22:36:35 +0200

    Création d'un package, histoire de faire les choses proprement. PS : Lancez le serveur avant le client si vous vous tester. Vous pouvez lancer le client plusieurs fois.

--- src/online/Serveur.java
+++ src/online/Serveur.java
@@ -6,9 +6,9 @@ import java.util.*;
 
 public class Serveur {
 	protected static final int port = 15000; // Port sur lequel se connecter
-	protected static ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'écoute du port
-	protected static Thread accepte_connexion; // Déclaration d'un thread
-	protected ArrayList<Socket> listeSocket; // Liste contenant les infos réseau
+	protected static ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'ï¿½coute du port
+	protected static Thread accepte_connexion; // Dï¿½claration d'un thread
+	protected ArrayList<Socket> listeSocket; // Liste contenant les infos rï¿½seau
 	protected ArrayList<String> listeJoueurs; //Liste contenant les identifiants
 	protected int nbJoueurs = 0; 
 	
@@ -34,75 +34,75 @@ public class Serveur {
 	public static void main(String[] args) {
 		
 		try {
-			socketServeur = new ServerSocket(port); // Création d'un socket d'écoute sur le port donnée
+			socketServeur = new ServerSocket(port); // Crï¿½ation d'un socket d'ï¿½coute sur le port donnï¿½e
 			Serveur serveur = new Serveur(); // Initialisation des ArrayList
-			System.out.println("Le serveur est à l'écoute du port "+socketServeur.getLocalPort());
+			System.out.println("Le serveur est ï¿½ l'ï¿½coute du port "+socketServeur.getLocalPort());
 			
-			/* Création d'un nouveau thread appelé accepte_connexion, dont la tâche à effectuer (la méthode run() ) 
+			/* Crï¿½ation d'un nouveau thread appelï¿½ accepte_connexion, dont la tï¿½che ï¿½ effectuer (la mï¿½thode run() ) 
 			 * se trouve dans la classe Accepter_connexion
-			 * qui est elle-même une implémentation de runnable (voir tuto openclassroom)
+			 * qui est elle-mï¿½me une implï¿½mentation de runnable (voir tuto openclassroom)
 			 */
 			accepte_connexion = new Thread(serveur.new Accepter_connexion(socketServeur));
 			accepte_connexion.start();
 				
 		} catch (IOException e) {
-			System.err.println("Le port " + socketServeur.getLocalPort() + " est déjà utilisé !");
+			System.err.println("Le port " + socketServeur.getLocalPort() + " est dï¿½jï¿½ utilisï¿½ !");
 		}
 		
 	}
 		
 	
-	/*Classe membre qui contient ce que doit exécuter le thread
-	 *Si ça vous pertubre, vous pouvez créer un nouveau fichier et faire un couper/coller.
-	 *Si vous faites ça, n'oubliez pas la portée des variables.
+	/*Classe membre qui contient ce que doit exï¿½cuter le thread
+	 *Si ï¿½a vous pertubre, vous pouvez crï¿½er un nouveau fichier et faire un couper/coller.
+	 *Si vous faites ï¿½a, n'oubliez pas la portï¿½e des variables.
 	 */
 	class Accepter_connexion implements Runnable{
 	
-		private ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'écoute du port
-		private Socket socketClient = null; // Socket qui s'occupe de l'échange de flux avec le client
+		private ServerSocket socketServeur = null; // Socket qui s'occupe SEULEMENT de l'ï¿½coute du port
+		private Socket socketClient = null; // Socket qui s'occupe de l'ï¿½change de flux avec le client
 		private PrintWriter sortie = null;
-		private BufferedReader entree = null; // L'entrée sera (presque) toujours lue en utilisant un buffer
+		private BufferedReader entree = null; // L'entrï¿½e sera (presque) toujours lue en utilisant un buffer
 		private String identifiant = null;
 		
-		// Récupération du socket de serveur de la classe Serveur
+		// Rï¿½cupï¿½ration du socket de serveur de la classe Serveur
 		public Accepter_connexion(ServerSocket ss) {
 			socketServeur = ss;
 		}
 		
-		// Démarrage du thread accepte_connexion
+		// Dï¿½marrage du thread accepte_connexion
 		public void run() {
 			
 			try {
-				while(nbJoueurs < 5){ // Normalement c'est un while(true), ou un autre prédicat
+				while(nbJoueurs < 5){ // Normalement c'est un while(true), ou un autre prï¿½dicat
 					
 					socketClient = socketServeur.accept(); // On accepte la connexion
 					
 					entree = new BufferedReader(new InputStreamReader(socketClient.getInputStream())); //Initialisation du buffer sur le socketClient
 					sortie = new PrintWriter(socketClient.getOutputStream()); // Initialisation du flux de sortie
 					
-					listeSocket.add(socketClient); // On ajoute le socket dans la liste (peut être utile pour la suite)
+					listeSocket.add(socketClient); // On ajoute le socket dans la liste (peut ï¿½tre utile pour la suite)
 					
 					identifiant = entree.readLine(); // On lit le sortie.println du Client
 					
 					listeJoueurs.add(identifiant); // On ajoute l'identifiant du client dans la liste
 					
-					System.out.println(identifiant+" vient de se connecter"); // Affichage d'un message côté Serveur
+					System.out.println(identifiant+" vient de se connecter"); // Affichage d'un message cï¿½tï¿½ Serveur
 					/* Si on voulait envoyer un message chez le client, on aurait fait :
-					 * sortie.println("blabla") côté Serveur
-					 * sortie.flush() côté Serveur pour vider le buffer
-					 * message = entree.readLine() côté Client
+					 * sortie.println("blabla") cï¿½tï¿½ Serveur
+					 * sortie.flush() cï¿½tï¿½ Serveur pour vider le buffer
+					 * message = entree.readLine() cï¿½tï¿½ Client
 					 */
 					
 					nbJoueurs++;
 				}
 				afficheSocket(listeSocket);
 				afficheLogin(listeJoueurs);
-				System.out.println("Il y a "+nbJoueurs+" joueurs connectés"); 
+				System.out.println("Il y a "+nbJoueurs+" joueurs connectï¿½s"); 
 				
 				
-				/* Fermeture du socket à la FIN du programme. Obligatoire pour libérer le port.
-				 * Cette méthode n'est utile que si on souhaite fermer le serveur, 
-				 * ce qui serait rare dans des conditions réelles */
+				/* Fermeture du socket ï¿½ la FIN du programme. Obligatoire pour libï¿½rer le port.
+				 * Cette mï¿½thode n'est utile que si on souhaite fermer le serveur, 
+				 * ce qui serait rare dans des conditions rï¿½elles */
 				socketServeur.close(); 
 				
 				
