f12231d688016560a4bb8f993d3de3f7f80e012a
GIorfindel <florian.choiselle@free.fr>
2015-04-24 20:45:36 +0200

    Attention aux majuscules et aux types de renvoie

--- src/mastermind/Solo.java
+++ src/mastermind/Solo.java
@@ -3,14 +3,14 @@ package mastermind;
 public class Solo extends Partie{
 	
 	/* Variables */
-	private Integer Coups; //le nombre de coup que le joueur a fait
+	private Integer coups; //le nombre de coup que le joueur a fait
 	private Tour tour; //le tour que l’on fait
 	
 public void setCoups( Integer coups ) {
 	this.coups = coups;
 	}
 	
-public Coups getCoups() {
+public Integer getCoups() {
 	return this.coups;
 	}
 	
