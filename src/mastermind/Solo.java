3a96dd4af2b1b0bb416cf3f10910f246f09e460a
ben <ben@debianBen>
2015-04-27 14:32:07 +0200

    Ajout de la variable qui compte le nombre de tour que l'on a fait

--- src/mastermind/Solo.java
+++ src/mastermind/Solo.java
@@ -4,10 +4,12 @@ public class Solo extends Partie{
 	
 	/* Variables */
 	private Integer coups; //le nombre de coup que le joueur a fait
+	private int nbTour;
 	private Tour tour; //le tour que l’on fait
 	
 public void setCoups( int coups ) {
 	this.coups = coups;
+	this.nbTour = 0;
 	}
 	
 public int getCoups() {
@@ -21,5 +23,13 @@ public void setTour( Tour tour ) {
 public Tour getTour() {
 	return this.tour;
 	}
+
+public int getNbTour(){
+	return this.nbTour;
+}
+
+public void setNbTour(){
+	this.nbTour ++;
+}
 
 }
