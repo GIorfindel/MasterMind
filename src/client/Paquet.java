a06960bec7b60d344b3914cdd6e612a03f2651e4
ben <ben@debianBen>
2015-04-28 21:44:17 +0200

    Correct erreur

--- src/client/Paquet.java
+++ src/client/Paquet.java
@@ -6,8 +6,8 @@ public class Paquet implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Object[] objets;
 	private int nbObjet;
-	static final int DEMANDE_CONNECTION = 0, REPONSE_CONNECTION = 1 ;
-	static final int DEMANDE_INSCRIPTION = 2, REPONSE_INSCRIPTION = 3 ;
+	public static final int DEMANDE_CONNECTION = 0, REPONSE_CONNECTION = 1 ;
+	public static final int DEMANDE_INSCRIPTION = 2, REPONSE_INSCRIPTION = 3 ;
 	private int type;
 	
 	public Paquet( int nbObjet, int type ){
