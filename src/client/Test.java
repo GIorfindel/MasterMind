0b121137ccae3f1d98e04b8683ff62c23323fe31
ben <ben@debianBen>
2015-04-29 19:54:25 +0200

    Ajout du Test du client

---
+++ src/client/Test.java
@@ -0,0 +1,14 @@
+package client;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client c = new Client( "192.168.0.16", 15000 );
+		c.initClient();
+	}
+
+}
