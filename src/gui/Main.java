3405b8e2577aa73a31159cd0ca7c531f9dbbf46e
ChristGood <christ.good@yahoo.fr>
2015-05-31 20:55:47 +0200

    fichiers bat pour windows

--- src/gui/Main.java
+++
@@ -1,7 +0,0 @@
-package gui;
-
-public class Main {
-public static void main(String[] args){
-	FenetreBis fen= new FenetreBis();
-}
-}
--- src/gui/Test.java
+++
@@ -1,9 +0,0 @@
-package gui;
-import javax.swing.JFrame;
- 
-public class Test extends JFrame {
-	
-  public static void main (String[] args) {
-	  Fenetre fenetre = new Fenetre();
-  }
-}
